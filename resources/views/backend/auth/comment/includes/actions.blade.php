2fb796c3cb6ae6a036cc2e2222e63f4e695f8d26
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-18 13:09:09 +0300

    creates forward link

--- resources/views/backend/auth/comment/includes/actions.blade.php
+++ resources/views/backend/auth/comment/includes/actions.blade.php
@@ -1,10 +1,13 @@
 @if ($comment)
     <div class="btn-group btn-group-sm" role="group" aria-label="@lang('labels.backend.access.users.user_actions')">
-        <a href="{{ route('admin.auth.comment.edit', $comment) }}" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.crud.edit')">
+        <a href="{{ route('admin.auth.comment.edit', $comment) }}" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.crud.edit')" class="btn btn-primary" >
             <i class="fas fa-edit"></i>
         </a>
         <a href="{{ route('admin.auth.comment.show', $comment) }}" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.crud.view')" class="btn btn-info">
             <i class="fas fa-eye"></i>
+        </a>
+        <a href="{{ route('admin.auth.comment.forward', $comment) }}" data-toggle="tooltip" data-placement="top" title="@lang('buttons.general.forward')" class="btn btn-dark">
+            <i class="fas fa-paper-plane"></i>
         </a>
     </div>
 @else
