8a43bc47a0d47804ced1c70bd7b0ec7848d8059d
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-18 11:45:41 +0300

    addes methods create, store, edit and update

--- app/Http/Controllers/Backend/Auth/Comment/CommentController.php
+++ app/Http/Controllers/Backend/Auth/Comment/CommentController.php
@@ -3,9 +3,95 @@
 namespace App\Http\Controllers\Backend\Auth\Comment;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Backend\Auth\Comment\ManageCommentRequest;
+use App\Http\Requests\Backend\Auth\Comment\StoreCommentRequest;
+use App\Http\Requests\Backend\Auth\Comment\UpdateCommentRequest;
+use App\Models\Auth\Comment;
+use App\Repositories\Backend\Auth\CommentRepository;
 
 class CommentController extends Controller
 {
-    //
+    
+     /**
+     * @var commentRepository
+     */
+    protected $commentRepository;
+
+    public function __construct(CommentRepository $commentRepository)
+    {
+        $this->commentRepository = $commentRepository;
+    }
+
+
+    public function index(ManageCommentRequest $request)
+    {
+        return view('backend.auth.comment.index')
+            ->withComments($this->commentRepository->orderBy('id')->paginate());
+    }
+        /**
+     * @param ManageCommentRequest $request
+     *
+     * @return mixed
+     */
+    public function create(ManageCommentRequest $request)
+    {
+        return view('backend.auth.comment.create');
+    }    
+    
+    /**
+    * @param  StoreCommentRequest  $request
+    *
+    * @return mixed
+    * @throws \App\Exceptions\GeneralException
+    * @throws \Throwable
+    */
+   public function store(StoreCommentRequest $request)
+   {
+       $this->commentRepository->create($request->only(
+            'first_name', 
+            'last_name', 
+            'phone', 
+            'message', 
+            'department', 
+            'user_id'
+        ));
+
+       return redirect()->route('admin.auth.comment.index')->withFlashSuccess(__('alerts.backend.comments.created'));
+   }  
+   
+   /**
+    * @param ManageCommentRequest $request
+    * @param Comment              $comment
+    *
+    * @return mixed
+    */
+   public function edit(ManageCommentRequest $request, Comment $comment)
+   {
+       return view('backend.auth.comment.edit')
+           ->withComment($comment);
+   }  
+    
+   /**
+   * @param  UpdateCommentRequest  $request
+   *
+   * @return mixed
+   * @throws \App\Exceptions\GeneralException
+   * @throws \Throwable
+   */
+  public function update(UpdateCommentRequest $request, Comment $comment)
+  {
+      $this->commentRepository->update($comment, $request->only(
+            'status', 
+            'department', 
+            'last_update_by'
+       ));
+
+      return redirect()->route('admin.auth.comment.index')->withFlashSuccess(__('alerts.backend.comments.updated'));
+  }
+  public function show(ManageCommentRequest $request, Comment $comment)
+  {
+      return view('backend.auth.comment.show')
+          ->withComment($comment);
+  }
+
 }
