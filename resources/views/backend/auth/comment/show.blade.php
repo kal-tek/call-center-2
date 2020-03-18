f282b21030e640def7603be82b5e33a2e5379afa
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-18 11:47:45 +0300

    creates show view file

---
+++ resources/views/backend/auth/comment/show.blade.php
@@ -0,0 +1,41 @@
+@extends('backend.layouts.app')
+
+@section('title', __('labels.backend.access.comment.management') . ' | ' . __('labels.backend.access.comment.view'))
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <div class="row">
+            <div class="col-sm-5">
+                <h4 class="card-title mb-0">
+                    @lang('labels.backend.access.comment.management')
+                    <small class="text-muted">@lang('labels.backend.access.comment.view')</small>
+                </h4>
+            </div><!--col-->
+        </div><!--row-->
+
+        <div class="row mt-4 mb-4">
+            <div class="col">
+                <ul class="nav nav-tabs" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" data-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-expanded="true"><i class="fas fa-comment"></i> @lang('labels.backend.access.comment.tabs.titles.overview')</a>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="overview" role="tabpanel" aria-expanded="true">
+                        @include('backend.auth.comment.show.tabs.overview')
+                    </div><!--tab-->
+                </div><!--tab-content-->
+            </div><!--col-->
+        </div><!--row-->
+    </div><!--card-body-->
+
+    <div class="card-footer">
+        <div class="row">
+            <div class="col">
+            </div><!--col-->
+        </div><!--row-->
+    </div><!--card-footer-->
+</div><!--card-->
+@endsection
