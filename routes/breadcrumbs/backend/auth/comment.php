e870f36b3b803b83f115a2c40b78f7084b67af46
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-18 13:03:10 +0300

    addes note attribute as fillable

--- routes/breadcrumbs/backend/auth/comment.php
+++ routes/breadcrumbs/backend/auth/comment.php
@@ -17,3 +17,7 @@ Breadcrumbs::for('admin.auth.comment.show', function ($trail, $id) {
     $trail->parent('admin.auth.comment.index');
     $trail->push(__('menus.backend.access.comments.show'), route('admin.auth.comment.show', $id));
 });
+Breadcrumbs::for('admin.auth.comment.forward', function ($trail, $id) {
+    $trail->parent('admin.auth.comment.index');
+    $trail->push(__('menus.backend.access.comments.forward'), route('admin.auth.comment.forward', $id));
+});
