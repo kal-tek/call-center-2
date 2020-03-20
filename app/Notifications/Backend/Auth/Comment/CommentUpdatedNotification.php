0a53a188bd6fd9144bc2be2cdcfd2a1d5b62d681
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-20 10:17:44 +0300

    renames comment notification classes

--- app/Notifications/Backend/Auth/Comment/CommentUpdatedNotification.php
+++ app/Notifications/Backend/Auth/Comment/CommentUpdatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class commentUpdated extends Notification
+class CommentUpdatedNotification extends Notification
 {
     use Queueable;
 
