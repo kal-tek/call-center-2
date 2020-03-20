1f31e7ef0f35999cd088573f68ba73a543adae36
Kalkidan Teklu <Kalkidanteklu4@gmail.com>
2020-03-20 10:05:08 +0300

    creates notification event

---
+++ app/Notifications/Backend/Auth/Comment/CommentCreatedNotification.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications\Backend\Auth\Comment;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Auth\Comment;
+use App\Models\Auth\User;
+class commentCreatedNotification extends Notification
+{
+    use Queueable;
+
+    protected $comment;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(Comment $comment)
+    {
+        //
+        $this->comment = $comment;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toDatabase($notifiable)
+    {
+        return [
+            'tittle' => 'comment created ',
+            'comment_id' => $this->comment->id,
+            'created_by' =>User::find($this->comment->user_id)->email,
+            'department' => $this->comment->department
+
+            //
+        ];
+    }
+}
